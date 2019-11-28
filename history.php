ee867209d3f82415cd6e2e82654742b8ab1405e5
lijunpeng-123 <57472166+lijunpeng-123@users.noreply.github.com>
2019-11-27 21:17:10 -0800

    Add files via upload

---
+++ history.php
@@ -0,0 +1,21 @@
+
+<?php
+//grabs function files 
+include("functions.php");
+//creates html header (see functions.php) TEST COMMENT
+make_header(); 
+?>
+
+<form>
+<li><span class="Year">2004</span><span class="Incident"> Jejep company established in FL</span></li>
+								<li><span class="Year">2018</span><span class="Incident">Become the Best 50 company<br>
+																	<li><span class="Year">2019</span><span class="Incident">Move to CA</span></li>
+
+</form>
+
+
+
+<?php
+//creates html footer (see functions.php)
+make_footer();
+?>
